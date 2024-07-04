fed81fe860b6cf6af5be4edf0071f192ff039421
Matrixbenny <mwangi.benvictor@strathmore.edu>
2024-07-04 15:19:04 +0300

    hulu.php

--- hulu.php
+++ hulu.php
@@ -1,65 +1,25 @@
+<!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="UTF-8" />
     <link rel="stylesheet" href="CSS/style.css">
-    <title style="font-style: unset;
-    text-decoration: blue;">BENNTORRENT/Nintendoswitch GAMES🎥</title>
+    <title>BENNTORRENT/Nintendoswitch GAMES</title>
 </head>
 
 <body>
     <div>
         <header>
-            <details>
-                <summary class="dropdown-toggle"><span class="menu-icon"></span> Menu</summary>
-                <header>
-                    <img src="images/LOGO.png" alt="Header Image" class="header-image">
-                    <!-- Your header content here -->
-                </header>
-                <nav>
-                    <audio id="myAudio" controls loop>
-                        <source src="SL - Berlin (Lyrics).mp3">
-                    </audio>
-
-                    <style>
-                        #myAudio {
-                            background-color: rgb(0, 91, 255);
-                            width: 200px;
-                            height: 50px;
-                        }
-                    </style>
-
-                    <div style="position: relative;">
-                        <button style="background-color: black;"><a href="index.html" style="color: white;">Home</a>
-                        </button>|
-                        <button style="background-color: black;"><a href="Games.html" style="color: white;">Games</a>
-                        </button>|<button style="background-color: black;"> <a href="Movies.html"
-                                style="color: white;">movies</a>
-                        </button>| <button style="background-color: black;"><a href="Tvshows.html"
-                                style="color: white;">Tvshows</a>
-                        </button>
-
-                        <button
-                            style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right:  50px;"><a
-                                href="Register.html" style="color: yellow;">Register</a>
-                        </button>|
-
-                        <button
-                            style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right: 0;"><a
-                                href="Login.html" style="color: yellow;">Login</a></button>
-
-                        <button class="email"
-                            style="color: mediumslateblue;background-color: black;border-radius:20px; align-content: end;"><a
-                                href="mailto:Officialbillionaire.ke@gmail.com ">Email for help</a>
-                        </button>
-                    </div>
-                </nav>
+            <!-- Your header content here -->
         </header>
+        <nav>
+            <!-- Your navigation menu here -->
+        </nav>
         <a href="index.html" style="font-size: 200%;"> 🔙</a>
         <p>
             <img src="images/hulu.jpeg">
             <br>
-        <h1 style="text-decoration: underline; width: fit-content;">Hulu Movies</h1>
+            <h1 style="text-decoration: underline; width: fit-content;">Hulu Movies</h1>
         </p>
         <style>
             .image-grid {
@@ -78,32 +38,11 @@
             }
         </style>
         <p class="image-grid">
-            <img src="images/h1.webp" />
-            <img src="images/h2.webp" />
-            <img src="images/h3.webp" />
-            <img src="images/h4.webp" />
-            <img src="images/h5.webp" />
-            <img src="images/h6.webp" />
-            <img src="images/h8.webp" />
-            <img src="images/h7.webp" />
-            <img src="images/h9.webp" />
-
-
-
+            <?php include 'movies.php'; ?>
         </p>
         <footer>
-            <div class="links-container">
-
-                <a href="helpcentre.html">Help centre</a>
-                <br>
-                <a href="carrers.html">Carrers</a>
-                <br>
-                <a href="termsofuse.html">Terms of use</a>
-                <br>
-                <a href="privacypolicy.html">Privacy Policy</a>
-            </div>
+            <!-- Your footer content here -->
         </footer>
-
-</body>
+    </body>
 
 </html>
