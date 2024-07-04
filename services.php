dba068aee07f7c7eb47ef17688d2a91715439eb3
Matrixbenny <mwangi.benvictor@strathmore.edu>
2024-07-04 16:22:47 +0300

    service.php

--- services.php
+++ services.php
@@ -1,51 +1,57 @@
-<!DOCTYPE html>
-<html>
+<?php
+// index.php
 
-<head>
-    <link rel="stylesheet" href="CSS/style.css">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-</head>
-<a href="index.html" style="font-size: 200%;"> 🔙</a>
+// Define the data for the table
+$data = array(
+    array("HBO MAX", "NINTENDO SWITCH", "RETURNING SHOWS"),
+    array("NETFLIX", "PC", "NEW RELEASES"),
+    array("HULU", "PS5", "UPCOMING RELEASES"),
+    array("PRIME VIDEO", "UPCOMING MOVIES", "NEW TVSHOWS")
+);
 
-<body>
-    <center>
-        <table border="1">
-            <thead>
-                <tr>
-                    <th>MOVIES</th>
-                    <th>GAMES</th>
-                    <th>TVSHOWS</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td><?php echo "HBO MAX"; ?></td>
-                    <td><?php echo "NINTENDO SWITCH"; ?></td>
-                    <td><?php echo "RETURNING SHOWS"; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo "NETFLIX"; ?></td>
-                    <td><?php echo "PC"; ?></td>
-                    <td><?php echo "NEW RELEASES"; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo "HULU"; ?></td>
-                    <td><?php echo "PS5"; ?></td>
-                    <td><?php echo "UPCOMING RELEASES"; ?></td>
-                </tr>
-                <tr>
-                    <td><?php echo "PRIME VIDEO"; ?></td>
-                    <td><?php echo "UPCOMING MOVIES"; ?></td>
-                    <td><?php echo "NEW TVSHOWS"; ?></td>
+// Start the HTML document
+echo '<!DOCTYPE html>';
+echo '<html>';
 
-                </tr>
-            </tbody>
-        </table>
-    </center>
+// Head section
+echo '<head>';
+echo '<link rel="stylesheet" href="CSS/style.css">';
+echo '<meta charset="utf-8">';
+echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">';
+echo '<title>Entertainment Hub</title>'; // Add a title to the page
+echo '</head>';
 
-    <script src=""></script>
-</body>
+// Back to index link
+echo '<a href="index.html" style="font-size: 200%;"> 🔙</a>';
 
-</html>
+// Body section
+echo '<body>';
+echo '<center>';
+echo '<table border="1">';
+echo '<thead>';
+echo '<tr>';
+echo '<th>MOVIES</th>';
+echo '<th>GAMES</th>';
+echo '<th>TVSHOWS</th>';
+echo '</tr>';
+echo '</thead>';
+echo '<tbody>';
+
+// Loop through the data and generate the table rows
+foreach ($data as $row) {
+    echo '<tr>';
+    echo '<td>' . $row[0] . '</td>';
+    echo '<td>' . $row[1] . '</td>';
+    echo '<td>' . $row[2] . '</td>';
+    echo '</tr>';
+}
+
+echo '</tbody>';
+echo '</table>';
+echo '</center>';
+
+// Script tag (no src attribute, so it won't load any script)
+echo '<script src="" async defer></script>';
+
+echo '</body>';
+echo '</html>';
