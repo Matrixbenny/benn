ef7682d4eea5bcdca235d52985f2da663b52601d
Matrixbenny <mwangi.benvictor@strathmore.edu>
2024-07-04 15:46:46 +0300

    privacypolicy.php

--- privacypolicy.php
+++ privacypolicy.php
@@ -1,179 +1,81 @@
-<!DOCTYPE html>
-<html>
+<?php
+// Configuration
+$company_name = "Your Company Name";
+$email_address = "Officialbillionaire.ke@gmail.com";
+$date = "2023-02-20"; // Effective date of the privacy policy
 
-<head>
-    <link rel="stylesheet" href="CSS/style.css">
-    <title>Privacy Policy</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
+// Header
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head>";
+echo "<link rel='stylesheet' href='CSS/style.css'>";
+echo "<title>Privacy Policy</title>";
+echo "<style>";
+echo "body { font-family: Arial, sans-serif; }";
+echo "h1, h2 { color: #0055aa; }";
+echo "p { text-align: justify; }";
+echo "</style>";
+echo "</head>";
 
-        h1,
-        h2 {
-            color: #0055aa;
-        }
+// Body
+echo "<body>";
+echo "<div>";
+echo "<header>";
+echo "<details>";
+echo "<summary class='dropdown-toggle'><span class='menu-icon'></span> Menu</summary>";
+echo "<header>";
+echo "<img src='images/LOGO.png' alt='Header Image' class='header-image'>";
+// Your header content here
+echo "</header>";
+echo "<nav>";
+echo "<audio id='myAudio' controls loop>";
+echo "<source src='SL - Berlin (Lyrics).mp3'>";
+echo "</audio>";
 
-        p {
-            text-align: justify;
-        }
-    </style>
-</head>
+echo "<style>";
+echo "#myAudio { background-color: rgb(0, 91, 255); width: 200px; height: 50px; }";
+echo "</style>";
 
-<body>
-    <div>
-        <header>
-            <details>
-                <summary class="dropdown-toggle"><span class="menu-icon"></span> Menu</summary>
-                <header>
-                    <img src="images/LOGO.png" alt="Header Image" class="header-image">
-                    <!-- Your header content here -->
-                </header>
-                <nav>
-                    <audio id="myAudio" controls loop>
-                        <source src="SL - Berlin (Lyrics).mp3">
-                    </audio>
+echo "<div style='position: relative;'>";
+echo "<button style='background-color: black;'><a href='index.html' style='color: white;'>Home</a></button>|";
+echo "<button style='background-color: black;'><a href='Games.html' style='color: white;'>Games</a></button>|";
+echo "<button style='background-color: black;'><a href='Movies.html' style='color: white;'>Movies</a></button>|";
+echo "<button style='background-color: black;'><a href='Tvshows.html' style='color: white;'>Tvshows</a></button>";
 
-                    <style>
-                        #myAudio {
-                            background-color: rgb(0, 91, 255);
-                            width: 200px;
-                            height: 50px;
-                        }
-                    </style>
+echo "<button style='background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right: 50px;'>";
+echo "<a href='Register.html' style='color: yellow;'>Register</a>";
+echo "</button>|";
 
-                    <div style="position: relative;">
-                        <button style="background-color: black;"><a href="index.html" style="color: white;">Home</a>
-                        </button>|
-                        <button style="background-color: black;"><a href="Games.html" style="color: white;">Games</a>
-                        </button>|<button style="background-color: black;"> <a href="Movies.html"
-                                style="color: white;">movies</a>
-                        </button>| <button style="background-color: black;"><a href="Tvshows.html"
-                                style="color: white;">Tvshows</a>
-                        </button>
+echo "<button style='background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right: 0;'>";
+echo "<a href='Login.html' style='color: yellow;'>Login</a>";
+echo "</button>";
 
-                        <button
-                            style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right:  50px;"><a
-                                href="Register.html" style="color: yellow;">Register</a>
-                        </button>|
+echo "<button class='email' style='color: mediumslateblue;background-color: black;border-radius:20px; align-content: end;'>";
+echo "<a href='mailto:$email_address'>Email for help</a>";
+echo "</button>";
+echo "</div>";
+echo "</nav>";
+echo "<br>";
+echo "</header>";
+echo "<a href='index.html' style='font-size: 200%;'> 🔙</a>";
+echo "<h1>Privacy Policy</h1>";
+echo "<p>This privacy policy sets out how $company_name uses and protects any information that you give $company_name when you use this website.</p>";
+echo "<p>$company_name is committed to ensuring that your privacy is protected. Should we ask you to provide certain information by which you can be identified when using this website, then you can be assured that it will only be used in accordance with this privacy statement.</p>";
+echo "<p>$company_name may change this policy from time to time by updating this page. You should check this page from time to time to ensure that you are happy with any changes. This policy is effective from $date.</p>";
 
-                        <button
-                            style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right: 0;"><a
-                                href="Login.html" style="color: yellow;">Login</a></button>
+echo "<h2>What we collect</h2>";
+echo "<p>We may collect the following information:</p>";
+echo "<ul>";
+echo "<li>Name and job title</li>";
+echo "<li>Contact information including email address</li>";
+echo "<li>Demographic information such as postcode, preferences and interests</li>";
+echo "<li>Other information relevant to customer surveys and/or offers</li>";
+echo "</ul>";
 
-                        <button class="email"
-                            style="color: mediumslateblue;background-color: black;border-radius:20px; align-content: end;"><a
-                                href="mailto:Officialbillionaire.ke@gmail.com ">Email for help</a>
-                        </button>
-                    </div>
-                </nav>
-                <br>
-        </header><a href="index.html" style="font-size: 200%;"> 🔙</a>
-        <h1>Privacy Policy</h1>
-        <p>This privacy policy sets out how [Your Company Name] uses and protects any information that you give [Your
-            Company Name] when you use this website.</p>
-        <p>[Your Company Name] is committed to ensuring that your privacy is protected. Should we ask you to provide
-            certain
-            information by which you can be identified when using this website, then you can be assured that it will
-            only be
-            used in accordance with this privacy statement.</p>
-        <p>[Your Company Name] may change this policy from time to time by updating this page. You should check this
-            page
-            from time to time to ensure that you are happy with any changes. This policy is effective from [date].</p>
-
-        <h2>What we collect</h2>
-        <p>We may collect the following information:</p>
-        <ul>
-            <li>Name and job title</li>
-            <li>Contact information including email address</li>
-            <li>Demographic information such as postcode, preferences and interests</li>
-            <li>Other information relevant to customer surveys and/or offers</li>
-        </ul>
-
-        <h2>What we do with the information we gather</h2>
-        <p>We require this information to understand your needs and provide you with a better service, and in particular
-            for
-            the following reasons:</p>
-        <ul>
-            <li>Internal record keeping.</li>
-            <li>We may use the information to improve our products and services.</li>
-            <li>We may periodically send promotional emails about new products, special offers or other information
-                which we
-                think you may find interesting using the email address which you have provided.</li>
-            <li>From time to time, we may also use your information to contact you for market research purposes. We may
-                contact you by email, phone, fax or mail. We may use the information to customise the website according
-                to
-                your interests.</li>
-        </ul>
-
-        <h2>Security</h2>
-        <p>We are committed to ensuring that your information is secure. In order to prevent unauthorised access or
-            disclosure, we have put in place suitable physical, electronic and managerial procedures to safeguard and
-            secure
-            the information we collect online.</p>
-
-        <h2>How we use cookies</h2>
-        <p>A cookie is a small file which asks permission to be placed on your computer's hard drive. Once you agree,
-            the
-            file is added and the cookie helps analyse web traffic or lets you know when you visit a particular site.
-            Cookies allow web applications to respond to you as an individual. The web application can tailor its
-            operations
-            to your needs, likes and dislikes by gathering and remembering information about your preferences.</p>
-        <p>We use traffic log cookies to identify which pages are being used. This helps us analyse data about web page
-            traffic and improve our website in order to tailor it to customer needs. We only use this information for
-            statistical analysis purposes and then the data is removed from the system.</p>
-        <p>Overall, cookies help us provide you with a better website, by enabling us to monitor which pages you find
-            useful
-            and which you do not. A cookie in no way gives us access to your computer or any information about you,
-            other
-            than the data you choose to share with us.</p>
-        <p>You can choose to accept or decline cookies. Most web browsers automatically accept cookies, but you can
-            usually
-            modify your browser setting to decline cookies if you prefer. This may prevent you from taking full
-            advantage of
-            the website.</p>
-
-        <h2>Links to other websites</h2>
-        <p>Our website may contain links to other websites of interest. However, once you have used these links to leave
-            our
-            site, you should note that we do not have any control over that other website. Therefore, we cannot be
-            responsible for the protection and privacy of any information which you provide whilst visiting such sites
-            and
-            such sites are not governed by this privacy statement. You should exercise caution and look at the privacy
-            statement applicable to the website in question.</p>
-
-        <h2>Controlling your personal information</h2>
-        <p>You may choose to restrict the collection or use of your personal information in the following ways:</p>
-        <ul>
-            <li>whenever you are asked to fill in a form on the website, look for the box that you can click to indicate
-                that you do not want the information to be used by anybody for direct marketing purposes</li>
-            <li>ifyou have previously agreed to us using your personal information for direct marketing purposes, you
-                may
-                change your mind at any time by writing to or emailing us at [email address]</li>
-        </ul>
-        <p>We will not sell, distribute or lease your personal information to third parties unless we have your
-            permission
-            or are required by law to do so.</p>
-        <p>You may request details of personal information which we hold about you under the Data Protection Act 1998. A
-            small fee will be payable.</p>
-        <p>If you believe that any information we are holding on you is incorrect or incomplete, please write to or
-            email us
-            as soon as possible, at [address or email address]. We will promptly correct any information found to be
-            incorrect.</p>
-
-        <footer>
-            <div class="links-container">
-
-                <a href="helpcentre.html">Help centre</a>
-                <br>
-                <a href="carrers.html">Carrers</a>
-                <br>
-                <a href="termsofuse.html">Terms of use</a>
-                <br>
-                <a href="privacypolicy.html">Privacy Policy</a>
-            </div>
-        </footer>
-</body>
-
-
-</html>
+echo "<h2>What we do with the information we gather</h2>";
+echo "<p>We require this information to understand your needs and provide you with a better service, and in particular for the following reasons:</p>";
+echo "<ul>";
+echo "<li>Internal record keeping.</li>";
+echo "<li>We may use the information to improve our products and services.</li>";
+echo "<li>We may periodically send promotional emails about new products, special offers or other information which we think you may find interesting using the email address which you have provided.</li>";
+echo "<li>From time to time, we may also use your information to contact you for market research purposes. We may contact you by email, phone, fax or mail. We may use the information to customise the website accordings
