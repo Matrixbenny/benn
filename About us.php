f785d571c12aa380e343692a9350fcdef6f0c1b0
Matrixbenny <mwangi.benvictor@strathmore.edu>
2024-07-04 14:46:34 +0300

    aboutus.php created

--- About us.php
+++ About us.php
@@ -1,142 +1,70 @@
-<!DOCTYPE html>
-<html>
+<?php
+// Header section
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '<link rel="stylesheet" href="CSS/style.css">';
+echo '<title>About Us</title>';
+echo '<style>';
+echo 'body {';
+echo '    font-family: Arial, sans-serif;';
+echo '    line-height: 1.6;';
+echo '    color: #333;';
+echo '    padding: 0 20px;';
+echo '}';
+echo 'h1, h2 {';
+echo '    color: #4a86e8;';
+echo '}';
+echo 'h1 {';
+echo '    font-size: 2.5em;';
+echo '    margin-bottom: 0;';
+echo '}';
+echo 'h2 {';
+echo '    font-size: 1.5em;';
+echo '    margin-top: 1em;';
+echo '    margin-bottom: 0.5em;';
+echo '}';
+echo 'p {';
+echo '    margin-bottom: 1em;';
+echo '}';
+echo '</style>';
+echo '</head>';
 
-<head>
-    <link rel="stylesheet" href="CSS/style.css">
-    <title>About Us</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            line-height: 1.6;
-            color: #333;
-            padding: 0 20px;
-        }
-
-        h1,
-        h2 {
-            color: #4a86e8;
-        }
-
-        h1 {
-            font-size: 2.5em;
-            margin-bottom: 0;
-        }
-
-        h2 {
-            font-size: 1.5em;
-            margin-top: 1em;
-            margin-bottom: 0.5em;
-        }
-
-        p {
-            margin-bottom: 1em;
-        }
-    </style>
-</head>
-
-<body>
-    <header>
-        <details>
-            <summary class="dropdown-toggle"><span class="menu-icon"></span> Menu</summary>
-
-            <header>
-                <img src="images/LOGO.png" alt="Header Image" class="header-image">
-                <!-- Your header content here -->
-            </header>
-            <nav>
-                <audio id="myAudio" controls loop>
-                    <source src="SL - Berlin (Lyrics).mp3">
-                </audio>
-
-                <style>
-                    #myAudio {
-                        background-color: rgb(0, 91, 255);
-                        width: 200px;
-                        height: 50px;
-                    }
-                </style>
-
-                <div style="position: relative;">
-                    <button style="background-color: black;"><a href="index.html" style="color: white;">Home</a>
-                    </button>|
-                    <button style="background-color: black;"><a href="Games.html" style="color: white;">Games</a>
-                    </button>|<button style="background-color: black;"> <a href="Movies.html"
-                            style="color: white;">movies</a>
-                    </button>| <button style="background-color: black;"><a href="Tvshows.html"
-                            style="color: white;">Tvshows</a>
-                    </button>
-
-                    <button
-                        style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right:  50px;"><a
-                            href="Register.html" style="color: yellow;">Signup</a>
-                    </button>|
-
-                    <button
-                        style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right: 0;"><a
-                            href="Login.html" style="color: yellow;">Signin</a></button>
-
-                    <button class="email"
-                        style="color: mediumslateblue;background-color: black;border-radius:20px; align-content: end;"><a
-                            href="mailto:Officialbillionaire.ke@gmail.com ">Email for help</a>
-                    </button>
-                </div>
-            </nav>
-    </header>
-    <br>
-    <a href="index.html" style="font-size: 200%;"> 🔙</a>
-    <h1>About Us</h1>
-    <p>Welcome to our website! We're excited to have you here and to share a little bit about ourselves.</p>
-    <p>We're a team of passionate professionals who are dedicated to providing the best possible products and
-        services
-        to our customers. We believe that by working together and leveraging our collective expertise, we can create
-        something truly special.</p>
-    <h2>Our Team</h2>
-    <p>Our team is made up of experts in a variety of fields, including design, development, marketing, and sales.
-        We're
-        always learning and growing, and we're committed to staying up-to-date with the latest trends and
-        technologies.
-    </p>
-    <h2>Our Culture</h2>
-    <p>We're proud of the culture we've created here, which is based on respect, collaboration, and a shared passion
-        for
-        what we do. We believe that by fostering a positive and inclusive work environment, we can bring out the
-        best in
-        each other and create something truly special.</p>
-    <h2>Our Community</h2>
-    <p>We're committed to giving back to our community and making a positive impact in the world. We regularly
-        donate
-        our time and resources to local charities and non-profits, and we're always looking for new ways to make a
-        difference.</p>
-    <p>Thank you for taking the time to learn more about us. We're excited to have you as a part of our community,
-        and
-        we can't wait to show you what we can do!</p>
-    <table>
-        <thead>
-            services we provide
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    Movies
-                </td>
-                <td Tvshows </td>
-            </tr>
-
-        </tbody>
-    </table>
-    <footer>
-        <div class="links-container">
-
-            <a href="helpcentre.html">Help centre</a>
-            <br>
-            <a href="carrers.html">Carrers</a>
-            <br>
-            <a href="termsofuse.html">Terms of use</a>
-            <br>
-            <a href="privacypolicy.html">Privacy Policy</a>
-        </div>
-    </footer>
-
-</body>
-
-</html>
+// Body section
+echo '<body>';
+echo '<header>';
+echo '<details>';
+echo '<summary class="dropdown-toggle"><span class="menu-icon"></span> Menu</summary>';
+echo '<header>';
+echo '<img src="images/LOGO.png" alt="Header Image" class="header-image">';
+// Your header content here
+echo '</header>';
+echo '<nav>';
+echo '<audio id="myAudio" controls loop>';
+echo '<source src="SL - Berlin (Lyrics).mp3">';
+echo '</audio>';
+echo '<style>';
+echo '#myAudio {';
+echo '    background-color: rgb(0, 91, 255);';
+echo '    width: 200px;';
+echo '    height: 50px;';
+echo '}';
+echo '</style>';
+echo '<div style="position: relative;">';
+echo '<button style="background-color: black;"><a href="index.html" style="color: white;">Home</a></button>|';
+echo '<button style="background-color: black;"><a href="Games.html" style="color: white;">Games</a></button>|';
+echo '<button style="background-color: black;"><a href="Movies.html" style="color: white;">movies</a></button>|';
+echo '<button style="background-color: black;"><a href="Tvshows.html" style="color: white;">Tvshows</a></button>';
+echo '<button style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right: 50px;"><a href="Register.html" style="color: yellow;">Signup</a></button>|';
+echo '<button style="background-color: darkmagenta; border-radius:20px;position: absolute;top: 0;right: 0;"><a href="Login.html" style="color: yellow;">Signin</a></button>';
+echo '<button class="email" style="color: mediumslateblue;background-color: black;border-radius:20px; align-content: end;"><a href="mailto:Officialbillionaire.ke@gmail.com ">Email for help</a></button>';
+echo '</div>';
+echo '</nav>';
+echo '</header>';
+echo '<br>';
+echo '<a href="index.html" style="font-size: 200%;"> 🔙</a>';
+echo '<h1>About Us</h1>';
+echo '<p>Welcome to our website! We\'re excited to have you here and to share a little bit about ourselves.</p>';
+echo '<p>We\'re a team of passionate professionals who are dedicated to providing the best possible products and services to our customers. We believe that by working together and leveraging our collective expertise, we can create something truly special.</p>';
+echo '<h2>Our Team</h2>';
+echo '<p>Our team is made up of experts in a variety of fields, including design, development, marketing, and sales. We\'re always learning and growing, and we\'
