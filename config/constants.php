66246328bb6f3959f767ee6b0be32717602a7b25
Matrixbenny <mwangi.benvictor@strathmore.edu>
2024-07-07 09:26:48 +0300

    constant code for connection

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,7 @@
 <?php
 //create PHP constants to enable connection to MySQL database
 //create constants
+//the code
 define('HOSTNAME','localhost');
 define('HOSTUSER','root');
 define('HOSTPASS','');
