b5ea595b8ef5bc701b96258417bc001ecf91069c
Matrixbenny <mwangi.benvictor@strathmore.edu>
2024-07-09 11:14:38 +0300

    deleted the connect folder

--- config/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Define database constants
-define('HOSTNAME', 'localhost');
-define('HOSTUSER', 'root');
-define('HOSTPASS', 'mysql'); // replace with your password if you have one
-define('DBNAME', 'webapp');
-?>
